696f1483b483c9dd5ad23950391ba4e576553367
peachops <chris.rutte@peachpayments.com>
2019-03-02 16:56:44 +0200

    added sorting and tests

--- imports/ui/NoteListHeader.js
+++ imports/ui/NoteListHeader.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data'
+import { Session } from 'meteor/session';
 
 // NoteListHeader
 
@@ -8,18 +9,24 @@ export const NoteListHeader = (props) => {
   return (
     <div>
       <button onClick={() => {
-        props.meteorCall('notes.insert');
+        props.meteorCall('notes.insert', (err, res) => {
+          if (res) {
+            props.Session.set('selectedNoteId', res);
+          }
+        });
       }}>Create Note</button>
     </div>
   );
 };
 
 NoteListHeader.propTypes = {
-  meteorCall: React.PropTypes.func.isRequired  
+  meteorCall: React.PropTypes.func.isRequired,
+  Session: React.PropTypes.object.isRequired
 }
 
 export default createContainer(() => {
   return {
-    meteorCall: Meteor.call
+    meteorCall: Meteor.call,
+    Session
   };
 }, NoteListHeader);
